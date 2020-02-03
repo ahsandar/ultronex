7ae03b303f71afc2b5d0f6c5dc22eaf58a255f01
Ahsan Dar <ahsan@wego.com>
2020-02-03 11:33:20 +0800

    fixed typo

--- bot/config/honeybadger.exs
+++ bot/config/honeybadger.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :honeybadger,
   api_key: System.get_env("HONEYBADGER_API_KEY"),
   environment_name: Mix.env(),
-  breadcrumbs_enabled: true
+  breadcrumbs_enabled: true,
   exclude_envs: [:test],
   use_logger: true
